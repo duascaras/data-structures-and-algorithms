107c750170c65a68c0d5fe730f7e79864954ed34
Gustavo Pereira <gustavodasilvapereira@gmail.com>
2024-10-18 17:41:20 -0300

    Stack

---
+++ README.md
@@ -0,0 +1,16 @@
+# Data Structures
+
+## Stack
+
+A data structure in which all insertions and deletions are mad at one end, called the top of the stack.
+
+LIFO data structure: last in, first out.
+
+### Common Stack Operations
+
+```cmd
+Push: push item to the top of the stack
+Pop: remove and return the top item
+Peek: return the top item without removing it
+Is_empty: return true if the stack is empty
+```
---
+++ stack.py
@@ -0,0 +1,53 @@
+"""
+Stack class
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+
+def reverse_string(my_string):
+    # Use the "accumulator pattern."
+    # Start with an "empty bucket" of the right data type,
+    # and build the solution by filling the bucket within a loop.
+    reversed_string = ""
+
+    # Create a new stack
+    s = Stack()
+
+    # Iterate through my_string and push the characters onto the stack
+    for char in my_string:
+        s.push(char)
+
+    # Use a while loop with the exit condition that the stack is empty.
+    # Within this loop, update reversed_string with characters popped off the stack.
+    while not s.is_empty():
+        reversed_string += s.pop()
+
+    # Return the result
+    return reversed_string
+
+
+result = reverse_string(
+    "!sedarg doog teg annoG\nssalc ni sessa rieht kcik annoG")
+print(result)
