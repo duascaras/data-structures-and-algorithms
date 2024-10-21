feb6630d5478da8bf1cedd7287bb73d7dabf8b39
Gustavo Pereira <gustavodasilvapereira@gmail.com>
2024-10-21 19:28:22 -0300

    Queue

---
+++ queue_algo.py
@@ -0,0 +1,28 @@
+"""
+Queue class
+"""
+
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
