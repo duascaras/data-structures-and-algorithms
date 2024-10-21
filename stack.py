26a48b0d1010028299556256ac6498619005718a
Gustavo Pereira <gustavodasilvapereira@gmail.com>
2024-10-21 19:16:32 -0300

    Made stack.py modular

--- stack.py
+++ stack.py
@@ -26,28 +26,28 @@ class Stack:
         return str(self.items)
 
 
-def reverse_string(my_string):
-    # Use the "accumulator pattern."
-    # Start with an "empty bucket" of the right data type,
-    # and build the solution by filling the bucket within a loop.
-    reversed_string = ""
+if __name__ == "__main__":
+    def reverse_string(my_string):
+        # Use the "accumulator pattern."
+        # Start with an "empty bucket" of the right data type,
+        # and build the solution by filling the bucket within a loop.
+        reversed_string = ""
 
-    # Create a new stack
-    s = Stack()
+        # Create a new stack
+        s = Stack()
 
-    # Iterate through my_string and push the characters onto the stack
-    for char in my_string:
-        s.push(char)
+        # Iterate through my_string and push the characters onto the stack
+        for char in my_string:
+            s.push(char)
 
-    # Use a while loop with the exit condition that the stack is empty.
-    # Within this loop, update reversed_string with characters popped off the stack.
-    while not s.is_empty():
-        reversed_string += s.pop()
+        # Use a while loop with the exit condition that the stack is empty.
+        # Within this loop, update reversed_string with characters popped off the stack.
+        while not s.is_empty():
+            reversed_string += s.pop()
 
-    # Return the result
-    return reversed_string
+        # Return the result
+        return reversed_string
 
-
-# result = reverse_string(
-#     "!sedarg doog teg annoG\nssalc ni sessa rieht kcik annoG")
-# print(result)
+    result = reverse_string(
+        "!sedarg doog teg annoG\nssalc ni sessa rieht kcik annoG")
+    print(result)
